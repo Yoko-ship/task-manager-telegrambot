58ce7d9f4bae1445df38a33f78f3002942ef657d
User <yuko.brandon13@gmail.com>
2024-12-11 18:20:02 +0500

    add

--- reminder.py
+++ reminder.py
@@ -38,7 +38,7 @@ create_table = """
 
 cursor.execute(user_table)
 cursor.execute(create_table)
-api_token = "7789911148:AAHgKryxshXcrnZTO8MyZJbCsXJ9b6_kmYE"
+api_token = os.getenv("API_KEY")
 bot = telebot.TeleBot(api_token)
 
 
